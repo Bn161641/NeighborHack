f9deaf1b397f3aa63e4ab19673cc35bb5d47ed62
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2022-11-12 16:24:34 -0600

    navbar

--- hackathon2022/src/components/Navbar.js
+++ hackathon2022/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import "./Navbar.css";
+import HomeIcon from '@mui/icons-material/Home';
+import PinDropIcon from '@mui/icons-material/PinDrop';
+import GroupsIcon from '@mui/icons-material/Groups';
+import Button from '@mui/material/Button';
 
 export default function Navbar(){
   return (<div className="navbar">
-    <img src="../../public/cooltext423335907473027.png" alt="logo" />
+    <img src={require('./cooltext423335907473027.png')} />
     <div className="navButtonContainer">
-      <h1>Hi</h1>
+      <Button size="large"><HomeIcon/></Button>
+      <Button size="large"><PinDropIcon/></Button>
+      <Button size="large"><GroupsIcon/></Button>
     </div>
     <div className="profileSection">
       
