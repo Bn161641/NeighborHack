56097e63549747715e6c59d97fb2e979526feae5
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2022-11-12 15:19:52 -0600

    clear

--- hackathon2022/src/App.js
+++ hackathon2022/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom"
 import './App.css';
+import { Home } from "./pages/Home";
+import { Discover } from "./pages/Discover";
+import { Profile } from "./pages/Profile";
+import { Navar } from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+<Route path="/" element={<Home />} />
