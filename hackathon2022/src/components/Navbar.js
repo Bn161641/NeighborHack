6656ae76f78b80cf09b4e827d2b84c7b49fbb191
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2022-11-12 17:27:35 -0600

    current iteration

--- hackathon2022/src/components/Navbar.js
+++ hackathon2022/src/components/Navbar.js
@@ -1,19 +1,39 @@
 import "./Navbar.css";
-import HomeIcon from '@mui/icons-material/Home';
-import PinDropIcon from '@mui/icons-material/PinDrop';
-import GroupsIcon from '@mui/icons-material/Groups';
-import Button from '@mui/material/Button';
+import HomeIcon from "@mui/icons-material/Home";
+import PinDropIcon from "@mui/icons-material/PinDrop";
+import GroupsIcon from "@mui/icons-material/Groups";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import SearchIcon from '@mui/icons-material/Search';
 
-export default function Navbar(){
-  return (<div className="navbar">
-    <img src={require('./cooltext423335907473027.png')} />
-    <div className="navButtonContainer">
-      <Button size="large"><HomeIcon/></Button>
-      <Button size="large"><PinDropIcon/></Button>
-      <Button size="large"><GroupsIcon/></Button>
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={require("./cooltext423335907473027.png")} />
+        <div class="searchBar">
+          <input
+            type="text"
+            class="searchBarInput"
+            placeholder="Search"
+          />
+          <button type="submit" class="searchBarButton">
+            <SearchIcon />
+          </button>
+        </div>
+      </div>
+      <div className="navButtonContainer">
+        <Button size="large">
+          <HomeIcon />
+        </Button>
+        <Button size="large">
+          <PinDropIcon />
+        </Button>
+        <Button size="large">
+          <GroupsIcon />
+        </Button>
+      </div>
+      <div className="profileSection"></div>
     </div>
-    <div className="profileSection">
-      
-    </div>
-  </div>);
-};
+  );
+}
