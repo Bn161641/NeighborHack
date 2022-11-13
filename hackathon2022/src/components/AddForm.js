ee733d9008c797070315a9db818a844436cb0096
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2022-11-13 08:57:11 -0600

    done

--- hackathon2022/src/components/AddForm.js
+++ hackathon2022/src/components/AddForm.js
@@ -1,3 +1,26 @@
+import "./AddForm.css"
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
+
 export default function AddForm(){
-  return (<div></div>);
+  const [type, setType] = useState("event")
+  return (
+  <div>
+    <form>
+    {/* <TextField
+          select
+          label="Select"
+          value={type}
+          onChange={handleChange}
+          helperText="Select Feed Type"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+    </form>
+  </div>);
 };
--- hackathon2022/src/components/Listing.js
+++ hackathon2022/src/components/Listing.js
@@ -2,11 +2,13 @@ import { useState, Component } from "react";
 import "./Listing.css";
 import Button from "@mui/material/Button";
 import SortIcon from "@mui/icons-material/Sort";
-import { feedData } from "../pages/feedData";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import IconButton from '@mui/material/IconButton';
+
 export default function Listing(props) {
   var dataUsing;
 
-  
   // if (props.nav === "showAll") {
   //   dataUsing = feedData;
   // } else if (props.nav === "events") {
@@ -18,7 +20,6 @@ export default function Listing(props) {
   // }
 
   // const [showListing, setShowListing] = useState(dataUsing);
-  
 
   return (
     <div className="discoverListing">
@@ -35,32 +36,57 @@ export default function Listing(props) {
       </div>
 
       <div className="ListOfCards">
-        {props.showListing.map(feed => {
-
-          if(feed.type === "event"){
-            return (<div className="CardContainer">
-          <div className="Card">
-            <img src={feed.image} />
-            <div className="CardBody">
-              <p className="date">{feed.date.when}</p>
-              <p className="title">{feed.title}</p>
-              <div className="attendeeAndLocatoin">
-                <p className="attendee">
-                  {feed.attendee} people interested
-                </p>
-                <p className="location">- &nbsp;{feed.address[1]}</p>
+        {props.showListing.map((feed) => {
+          if (feed.type === "event" || feed.type === "volunteer") {
+            return (
+              <div className="CardContainer">
+                <div className="Card">
+                  <img src={feed.image} />
+                  <div className="CardBody">
+                    <p className="date">{feed.date.when}</p>
+                    <p className="title">{feed.title}</p>
+                    <div className="attendeeAndLocatoin">
+                      <p className="attendee">
+                        {feed.attendee} people interested
+                      </p>
+                      <p className="location">- &nbsp;{feed.address[1]}</p>
+                    </div>
+                    <Button variant="text">Show on map</Button>
+                  </div>
+                  <div className="interestedContainer">
+                    <Button className="interestedButton" variant="outlined">
+                      interested
+                    </Button>
+                  </div>
+                </div>
               </div>
-              <Button variant="text">Show on map</Button>
-            </div>
-            <div className="interestedContainer">
-              <Button className="interestedButton" variant="outlined">
-                interested
-              </Button>
-            </div>
-          </div>
-        </div>);
+            );
+          } else if (feed.type === "report") {
+            return (
+              <div className="CardContainer">
+                <div className="Card">
+                  <img src={feed.image} />
+                  <div className="CardBody">
+                    <p className="date">{feed.date.when}</p>
+                    <p className="title">{feed.title}</p>
+                    <div className="attendeeAndLocatoin">
+                      <p className="attendee">{feed.attendee} people upvoted</p>
+                      <p className="location">- &nbsp;{feed.address[1]}</p>
+                    </div>
+                    <Button variant="text">Show on map</Button>
+                  </div>
+                  <div className="interestedContainer thumbContainer">
+                    <IconButton>
+                      <ThumbUpIcon />
+                    </IconButton>
+                    <IconButton>
+                      <ThumbDownIcon />
+                    </IconButton>
+                  </div>
+                </div>
+              </div>
+            );
           }
-          
         })}
       </div>
     </div>
