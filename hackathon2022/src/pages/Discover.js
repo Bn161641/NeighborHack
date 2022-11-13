a39d45b9d57d6f3b3080a45c2da7b531133640c1
ericmutton <64441308+ericmutton@users.noreply.github.com>
2022-11-13 03:27:18 -0600

    insert map

--- hackathon2022/src/pages/Discover.js
+++ hackathon2022/src/pages/Discover.js
@@ -1,12 +1,13 @@
 import "./Discover.css";
 import Filter from "../components/Filter";
 import Map from "../components/Map";
+import "../ununpkg/leafletjs-1.9.2.css";
 
 export default function Discover() {
   return (
     <div className="discover">
       <Filter />
-      <Map />
+      <div id="map"><Map /></div>
     </div>
   );
 }
