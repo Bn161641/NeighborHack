86ef18a91dff028fb3074c3cc483ec62142ccd0e
ericmutton <64441308+ericmutton@users.noreply.github.com>
2022-11-12 23:30:10 -0600

    import Map

--- hackathon2022/src/pages/Discover.js
+++ hackathon2022/src/pages/Discover.js
@@ -1,10 +1,12 @@
 import "./Discover.css";
 import Filter from "../components/Filter";
+import Map from "../components/Map";
 
 export default function Discover() {
   return (
     <div className="discover">
       <Filter />
+      <Map />
     </div>
   );
 }
