f6674fc4ec3da1b4e047b3d5e1a46d50515b0882
Matt Pham <matthewpham135@gmail.com>
2022-11-13 04:30:27 -0600

    home page functionality done

--- hackathon2022/src/data.js
+++ hackathon2022/src/data.js
@@ -33,6 +33,42 @@ export const data = [
         "profile":"profile1.jpg",
         "duration": "1 day ago",
         "type":"Report",
-        "id": 1
+        "id": 4
+    },
+    {
+        "author":"matthew pham",
+        "description":" It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"None",
+        "id": 5
+    },
+    {
+        "author":"matthew pham",
+        "description":"this app stinks!",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Event",
+        "id": 6
+    },
+    {
+        "author":"matthew pham",
+        "description":"this app stinks!",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Volunteering",
+        "id": 7
+    },
+    {
+        "author":"matthew pham",
+        "description":" It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Report",
+        "id": 8
     },
 ]
