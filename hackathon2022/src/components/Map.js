fd8fc7257452879c194ffbadcc74027dfbb8ebda
ericmutton <64441308+ericmutton@users.noreply.github.com>
2022-11-13 10:11:07 -0600

    crumbs

--- hackathon2022/src/components/Map.js
+++ hackathon2022/src/components/Map.js
@@ -1,22 +1,24 @@
 //import Leaflet from "../ununpkg/leafletjs-1.9.2";
 import "./Map.css";
+import { useState } from "react";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
-import { Marker } from "react-leaflet/Marker";
-import { Popup } from "react-leaflet/Popup";
+import { Circle } from "react-leaflet/Circle";
+import { PlaceMarkers } from "./PlaceMarkers";
 
 export default function Map(props) {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+    const [lat, setLat] = useState(32.9800)
+    const [long, setLong] = useState(-96.7400);
+    const [zoom, setZoom] = useState(15);
+    
+    return (
+        <MapContainer center={[lat, long]} zoom={zoom} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Circle center={[lat,long]} radius={props.range*220}/>
+            <PlaceMarkers toggle={props.toggle} />
+        </MapContainer>
+    );
 }
