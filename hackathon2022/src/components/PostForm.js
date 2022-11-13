28d6c9207de05b9d66aa67af00f219535086e02f
Matt Pham <matthewpham135@gmail.com>
2022-11-13 04:20:21 -0600

    finished home functionality

---
+++ hackathon2022/src/components/PostForm.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { CardActions, Chip } from '@mui/material';
+
+export default function PostForm(){
+    const [types, setTypes] = React.useState([
+        {text:'None', chosen: false,},
+        {text:'Event', chosen: false,},
+        {text:'Volunteer', chosen: false,},
+        {text:'Report', chosen: false,},
+      ]);
+    
+      const choices = types.map(choice => (
+        <Chip 
+            label={choice.text}
+            style={{backgroundColor: choice.chosen && "gray",
+            color: choice.chosen && "white"}}
+            onClick={() => handleClick(choice)}
+        />
+    ))
+
+
+    function handleClick(chosenType){
+        setTypes(oldTypes => oldTypes.map(oldType => {
+            return chosenType.text === oldType.text ?
+            {...oldType, chosen: !oldType.chosen} :
+            {...oldType, chosen: false}
+        }))
+    }
+    
+    return(
+        <div>
+            <Card sx={{ width: 500 }} style={{textAlign:'left', padding:'10px'}}>
+                <CardHeader
+            
+                        title={<TextField
+                            id="filled-multiline-static"
+                            label="What's Happening?"
+                            multiline
+                            rows={4}
+                            defaultValue=""
+                            variant="filled"
+                            fullWidth
+                          />}
+                    />
+                <CardActions>
+                    {choices}
+                    <Button variant="contained" style={{marginLeft:'auto'}}>
+                        Send
+                    </Button>
+                </CardActions>   
+            </Card>
+        </div>
+    )
+}
---
+++ hackathon2022/src/contactData.js
@@ -0,0 +1,14 @@
+export const contacts = [
+    {
+        "name":"matthew pham",
+        "profile":"profile1.jpg",
+        "message":"we are losing this hackathon",
+        "id": 1
+    },
+    {
+        "name":"matthew pham",
+        "profile":"profile1.jpg",
+        "message":"we are losing this hackathon",
+        "id": 2
+    },
+]
