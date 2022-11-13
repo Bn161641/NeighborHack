02657fbb7f72971e07b60eb62570030afc6c38c4
Bn161641 <60488475+Bn161641@users.noreply.github.com>
2022-11-12 22:57:02 -0600

    done with side bar

--- hackathon2022/src/components/Filter.js
+++ hackathon2022/src/components/Filter.js
@@ -2,14 +2,69 @@ import "./Filter.css";
 import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import AppsIcon from '@mui/icons-material/Apps';
-import EventIcon from '@mui/icons-material/Event';
-import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
-import ReportIcon from '@mui/icons-material/Report';
+import AppsIcon from "@mui/icons-material/Apps";
+import EventIcon from "@mui/icons-material/Event";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import ReportIcon from "@mui/icons-material/Report";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Slider from '@mui/material/Slider';
 
 export default function Filter() {
   const [range, setRange] = useState(20);
-  const [filterName, setFilterName] = useState("Richardson, TX - Within 20 miles")
+  const [filterName, setFilterName] = useState("Richardson, TX");
+  const [sideNav, setSideNav] = useState({
+    showAll: true,
+    events: false,
+    volunteers: false,
+    reports: false,
+  });
+
+  const handleSliderChange = (event, newValue) => {
+    setRange(newValue);
+  };
+
+  function changeView(view) {
+    switch (view) {
+      case "showAll":
+        setSideNav({
+          showAll: true,
+          events: false,
+          volunteers: false,
+          reports: false,
+        });
+        break;
+      case "events":
+        setSideNav({
+          showAll: false,
+          events: true,
+          volunteers: false,
+          reports: false,
+        });
+        break;
+      case "volunteers":
+        setSideNav({
+          showAll: false,
+          events: false,
+          volunteers: true,
+          reports: false,
+        });
+        break;
+      case "reports":
+        setSideNav({
+          showAll: false,
+          events: false,
+          volunteers: false,
+          reports: true,
+        });
+        break;
+    }
+  }
 
   return (
     <div className="filter">
@@ -23,17 +78,66 @@ export default function Filter() {
         />
       </div>
       <div className="discoverCategory">
-        <Button variant="text">
-          <div className="sideNavIcon"><AppsIcon/></div>
+        {/* <p>{sideNav}</p> */}
+        <Button
+          onClick={() => changeView("showAll")}
+          className={`${sideNav.showAll ? `active` : ``}`}
+          variant="text"
+        >
+          <div className={`sideNavIcon ${sideNav.showAll ? `active` : ``}`}>
+            <AppsIcon />
+          </div>
           Show All
-          </Button>
-        <Button variant="text"><div className="sideNavIcon"><EventIcon/></div>Events</Button>
-        <Button variant="text"><div className="sideNavIcon"><VolunteerActivismIcon/></div>Volunteers</Button>
-        <Button variant="text"><div className="sideNavIcon"><ReportIcon/></div>Reports</Button>
+        </Button>
+        <Button
+          onClick={() => changeView("events")}
+          className={`${sideNav.events ? `active` : ``}`}
+          variant="text"
+        >
+          <div className={`sideNavIcon ${sideNav.events ? `active` : ``}`}>
+            <EventIcon />
+          </div>
+          Events
+        </Button>
+        <Button
+          onClick={() => changeView("volunteers")}
+          className={`${sideNav.volunteers ? `active` : ``}`}
+          variant="text"
+        >
+          <div className={`sideNavIcon ${sideNav.volunteers ? `active` : ``}`}>
+            <VolunteerActivismIcon />
+          </div>
+          Volunteers
+        </Button>
+        <Button
+          onClick={() => changeView("reports")}
+          className={`${sideNav.reports ? `active` : ``}`}
+          variant="text"
+        >
+          <div className={`sideNavIcon ${sideNav.reports ? `active` : ``}`}>
+            <ReportIcon />
+          </div>
+          Reports
+        </Button>
       </div>
       <div className="discoverFilter">
         <h3 className="filterLabel">Filter</h3>
-        <Button variant="text">{filterName}</Button>
+        {/* <Button variant="text">{filterName}</Button> */}
+          <FormControl className="discoverFormController" variant="standard">
+            <Input
+              className="locationForm"
+              value={filterName}
+              startAdornment={
+                <InputAdornment position="start">
+                  <LocationOnIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <div className="discoverRange">
+            <p className="pRange">{range} miles</p>
+            <Slider onChange={handleSliderChange} defaultValue={range} aria-label="Default"/>
+          </div>
       </div>
     </div>
   );
