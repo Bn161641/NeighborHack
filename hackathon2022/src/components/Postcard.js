545c052cac3be2db5133016628b95994d8521ad3
Matt Pham <matthewpham135@gmail.com>
2022-11-12 23:26:04 -0600

    feed done

---
+++ hackathon2022/src/components/Postcard.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton, Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import CommentIcon from '@mui/icons-material/Comment';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Chip from '@mui/material/Chip';
+
+export default function Postcard({ post }){
+
+    return(
+        <div>
+            <Card sx={{ width: 500 }} style={{textAlign:'left',position:'relative'}}>
+                <CardHeader
+                    avatar={<Avatar src={require(`../assets/${post.profile}`)}/>}
+                    action={
+                        <div>
+                            {
+                            post.type === "Event" && <Chip 
+                            label={post.type} 
+                            style={{backgroundColor:"#4681f4", color:"white"}}
+                            >
+                            </Chip>
+                            } 
+                            {
+                            post.type === "Volunteering" && <Chip 
+                            label={post.type} 
+                            style={{backgroundColor:"#33b249", color:"white"}}
+                            >
+                            </Chip>
+                            } 
+                            {
+                                post.type === "Report" && <Chip 
+                                label={post.type} 
+                                style={{backgroundColor:"#f44336", color:"white"}}
+                                >
+                                </Chip>
+                            }
+                            <IconButton>
+                                <MoreVertIcon />
+                            </IconButton>
+                        </div>
+                    }
+                    title={<Typography style={{fontWeight:'bold'}}>{post.author}</Typography>}
+                    subheader={post.duration}
+                />
+                <CardContent>
+                    <Typography variant="body2">
+                        {post.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <IconButton aria-label="location" style={{marginRight:'190px'}}>
+                        <LocationOnIcon />
+                        <Typography variant="body2">
+                            {post.location}
+                        </Typography>
+                    </IconButton>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="comment">
+                        <CommentIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                </CardActions>         
+            </Card>
+        </div>
+    )
+}
---
+++ hackathon2022/src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        "author":"matthew pham",
+        "description":" It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"None",
+        "id": 1
+    },
+    {
+        "author":"matthew pham",
+        "description":"this app stinks!",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Event",
+        "id": 2
+    },
+    {
+        "author":"matthew pham",
+        "description":"this app stinks!",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Volunteering",
+        "id": 3
+    },
+    {
+        "author":"matthew pham",
+        "description":" It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "location":"123 Happy Street",
+        "profile":"profile1.jpg",
+        "duration": "1 day ago",
+        "type":"Report",
+        "id": 1
+    },
+]
